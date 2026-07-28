7133638de96387e7b5246793b06bfeebc4a95bdc
bala <subbu509@gmail.com>
2026-07-27 22:05:03 -0400

    first day

---
+++ src/ExecutorsDemo.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorsDemo {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+      ExecutorService executor = Executors.newFixedThreadPool(10);
+      ExecutorService executor1 = Executors.newCachedThreadPool();
+    Future<Integer> future=  executor.submit(()->{
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("Helooooooooo");
+        return 1;
+    });
+    //  Object o= future.get();
+     System.out.println(1+" is from Future Object");
+     executor.shutdown();
+    executor.awaitTermination(1, TimeUnit.SECONDS);
+      System.out.println("Main exit");
+    }
+}
